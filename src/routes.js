12814273912799450f639e4c752331670e62a37e
Yvan Kamgang <yvankamgang45@gmail.com>
2026-02-16 10:09:20 +0100

    16

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require('express')
 // Crée un router pour les routes de l'API
 const router = express.Router()
 
+
 // Route pour vérifier si l'API est fonctionnelle
 router.get('/health', (req, res) => {
     res.json({ status: 'ok' })
@@ -11,6 +12,12 @@ router.get('/health', (req, res) => {
 // Route pour retourner l'heure actuelle
 router.get('/time', (req, res) => {
     res.json({ time: new Date().toISOString() })
+})
+
+router.get('/welcome', (req, res) => {
+const name = req.query.name || 'Visiteur'
+// VULNÉRABILITÉ : On renvoie l'input utilisateur sans filtrage
+res.send(`<h1>Bienvenue, ${name}</h1>`)
 })
 
 // Exporte le router pour être utilisé dans l'application
